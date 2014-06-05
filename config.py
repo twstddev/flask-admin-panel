985a95af31617e0904255378bf60a333ff20d94c
twstddev <twstd.dev@gmail.com>
2014-06-05 22:17:14 +0000

    Add initial values to config and simple run instructions

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_PATH = os.path.abspath( os.path.dirname( __file__ ) )
+
+HOST = 0.0.0
+
+DEBUG = True
+
+SECRET_KEY = ""
+CSRF_SESSION_KEY = "yoursecretkey"
+
+# Database connection details go here.
+
