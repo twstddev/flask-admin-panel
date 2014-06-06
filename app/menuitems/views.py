43fe1acee58e0ef28fc1379e2ac76756373a771d
twstddev <twstd.dev@gmail.com>
2014-06-06 16:09:22 +0000

    Add menu item new form template

--- app/menuitems/views.py
+++ app/menuitems/views.py
@@ -5,3 +5,8 @@ module = Blueprint( "menuitems:admin", __name__, url_prefix = "/admin/menuitems"
 @module.route( "/" )
 def index():
 	return render_template( "menuitems/index.html" )
+
+@module.route( "/new" )
+def new():
+	return render_template( "menuitems/new.html" )
+	
